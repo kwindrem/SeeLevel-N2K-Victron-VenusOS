08346d057377fc082238f113fb421397f00d93af
kwindrem <58538395+kwindrem@users.noreply.github.com>
2020-03-17 13:07:25 -0700

    Updated SeeLevel dBus service polling frequency to avoid missing updates
    The SeeLevel N2K system often sends updates very close together. These update the SeeLevel dBus object which is polled my the repeater process. CheckSeeLevel () is the method that polls the dBus object. GetValue () calls take a significant amount of time so the code now checks to see if a new tank is being reported before processing the poll further. In addition, repeater services are updated only every 1 second.
    
    Before these changes, a tank would indicate "NO RESPONSE" for a long period of time, possibly requiring reset of the CCGX and/or SeeLevel.
    
    These changes are not perfect. Timeouts (missing a tank for 10 seconds or more) still occur -- about 0.8%. The good news is that the "NO REPSONSE" indications last only 1-2 seconds.

--- SeeLevelRepeater/SeeLevelRepeater.py
+++ SeeLevelRepeater/SeeLevelRepeater.py
@@ -54,7 +54,7 @@ from vedbus import VeDbusService
 # the name is determined by examining the system once the SeeLevel N2K sensor system is attached
 # NOTE: MUST BE SET MANUALLY IN THE CODE AFTER ACTUAL SERVICE NAME IS DETERMINED (use dbus-spy)
 
-SeeLevelServiceName = 'com.victronenergy.tank.socketcan_can0_di0_uc855'
+SeeLevelServiceName = 'com.victronenergy.tank.socketcan_can0_vi0_uc855'
 
 # RepeaterServiceName is the name of the dBus service where data is sent
 # instance number is filled in when the service is created
@@ -64,22 +64,20 @@ ProductName = 'SeeLevel Tank %d Repeater'
 
 # timer periods and watchdog timeout are defined here for convenience
 
-# Repeater timeout is 20 seconds
-# based on a cycle time of 6-8 seconds and time for one missed cycle
-# Repeater timeouts manage visibility of the associated tank information in the GUI
-# SeeLevel can turn off one or more tanks so we want to hide them when not reported
+# If a repater service is not updated at least every 10 seconds
+# it is marked as disconnected so the GUI can alert the user that
+# the level should not be trusted
 
-RepeaterTimeoutInSeconds = 20;
+RepeaterTimeoutInSeconds = 10
 
 # the update loop in the Repeater only manages timeouts so runs infrequently
 RepeaterTimerPeriodInSeconds = 1.0
 
 # the SeeLevel scan period is set to avoid missing tanks before SeeLevel moves on to the next tank
-# a shorter period takes additional CPU processing
-# too long a period may cause SeeLevel to overwrite the SeeLevel dBus service before data for one tank can be processed
-# 400 mS should allow 5 reads of the SeeLevel dBus service values before the next it is updated with the next tank
+# SeeLevel often reports tanks with little time between them so the scan interval must be very short
+# Testing indicates 50 mS between checks prevents missing a tank
 
-SeeLevelScanPeriodInSeconds = 0.4
+SeeLevelScanPeriodInSeconds = 0.05
 
 SeeLevelScanPeriod = int (SeeLevelScanPeriodInSeconds * 1000)		# in timer ticks
 RepeaterTimerPeriod = int (RepeaterTimerPeriodInSeconds * 1000)		# in timer ticks
@@ -118,8 +116,9 @@ class Repeater:
     DbusBus = None
     ServiceName = ""
 
-    TimeoutCount = 0
     Tank = 0
+    TimeoutCount = 0
+    DisconnectTime = 0
 
 
     def __init__(self, tank):
@@ -145,7 +144,10 @@ class Repeater:
 
 	if self.TimeoutCount > self.RepeaterTimeout:
 		if self.RepeaterService != None:
-			self.RepeaterService['/Connected'] = False
+			self.RepeaterService['/Connected'] = 0
+			self.DisconnectTime += 1
+####
+			self.RepeaterService['/HardwareVersion'] = self.DisconnectTime
 	else:
 		self.TimeoutCount += 1
 
@@ -186,13 +188,14 @@ class Repeater:
         self.RepeaterService.add_path ('/FirmwareVersion', 0)
         self.RepeaterService.add_path ('/HardwareVersion', 0)
         self.RepeaterService.add_path ('/Serial', '')
-# make /Connected writable
-	self.RepeaterService.add_path ('/Connected', False, writeable = True, onchangecallback = self._handlechangedvalue)
+# use numeric values (1/0) not True/False to make GUI display correct state
+	self.RepeaterService.add_path ('/Connected', 0)
  
 	self.RepeaterService.add_path ('/Level', 0, writeable = True, onchangecallback = self._handlechangedvalue)
 	self.RepeaterService.add_path ('/FluidType', self.Tank, writeable = True, onchangecallback = self._handlechangedvalue)
 	self.RepeaterService.add_path ('/Capacity', 0, writeable = True, onchangecallback = self._handlechangedvalue)
 	self.RepeaterService.add_path ('/Remaining', 0, writeable = True, onchangecallback = self._handlechangedvalue)
+####	self.RepeaterService.add_path ('/DisconnectTime', 0, writeable = True, onchangecallback = self._handlechangedvalue)
 
 	self.TimeoutCount = 0;
 
@@ -202,6 +205,7 @@ class Repeater:
 # method that is called from the SeeLevel loop to update repeater values
 # the first call to this method creates the service for this Repeater instance
 # preventing unnecessary dBus services from hanging around
+# update repeater service only if values change
 
     def UpdateRepeater (self, level, capacity):
 
@@ -211,7 +215,7 @@ class Repeater:
 	self.RepeaterService['/Level'] = level
 	self.RepeaterService['/Capacity'] = capacity
 	self.RepeaterService['/Remaining'] = capacity * level / 100
-	self.RepeaterService['/Connected'] = True
+	self.RepeaterService['/Connected'] = 1
 
 	self.TimeoutCount = 0;
 	return True
@@ -220,14 +224,15 @@ class Repeater:
 
 # CheckSeeLevel is the main processing loop to extract SeeLevel information
 # it collects and validates information from SeeLevel and forwards it to the tank repeater objects
-# Because this is done as a poll of changing data, validity must be tested before updating the Repeater service
-# Data from SeeLevel is read as quickly as possible followed by a second read of the first parameter
+# Data from SeeLevel is read as quickly as possible followed by a second read of the first parameter (tank number)
 # if that second read matches the first, we assume the intermediate reads are valid
-# if they do not match, a second attempt is made to read all values and another test of the first parameter is made
-# tests have shown a 20-100% failure rate when reading the parameter set once and typically under 2% when a second read is attempted immediately
+# if the second read does not match the first, processing is skipped until the next pass
 #
 # this method runs even if a SeeLevel sensor system isn't attached to Venus. In this case, the SeeLevel service won't exist
 # read attempts to that service will generate an exception which is trapped here to skip processing.
+#
+# This method runs frequently to avoid missed tanks, however normally, the same tank persists through multiple passes
+# to avoid excessive CPU load, tank information is only processed once per second
 
 # persistent storage for SeeLevel data are created
 # so objects don't have to be fetched each time this process runs
@@ -235,11 +240,17 @@ class Repeater:
 SeeLevelTankObject = None
 SeeLevelFluidLevelObject = None
 SeeLevelCapacityObject = None
+LastTank = 0
 
 # RepeaterList provides persistent storage for a repeater instance so that it may be called from CheckSeeLevel 
 # This list is indexed by fluid type and needs to be expanded if additional fluid types are added in the future
 
 RepeaterList =  [None,  None, None, None, None, None ]
+
+# LastUpdate keeps track of the time each tank repeater was updated
+# This list is indexed by fluid type and needs to be expanded if additional fluid types are added in the future
+
+LastUpdate =   [0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ]
 
 def CheckSeeLevel():
 
@@ -251,8 +262,8 @@ def CheckSeeLevel():
 	global SeeLevelCapacityObject
 
 	global RepeaterList
-
-	bus = dbus.SystemBus()
+	global LastTank
+	global LastUpdate
 
 	try:
 
@@ -261,32 +272,34 @@ def CheckSeeLevel():
 # if the SeeLevel service does not exist, a DBusException occurs and is handled below, skipping all processing here
 
             if SeeLevelTankObject == None:
+		bus = dbus.SystemBus()
 		SeeLevelTankObject = bus.get_object(SeeLevelServiceName, '/FluidType')
 		SeeLevelFluidLevelObject = bus.get_object(SeeLevelServiceName, '/Level')
 		SeeLevelCapacityObject = bus.get_object (SeeLevelServiceName, '/Capacity')
+	
+	    currentTime = time.time()
 
 # quickly grab a set of values
 	    tank = SeeLevelTankObject.GetValue()
+
+# range check tank to avoid out of bounds indexing
+	    if tank < 0 or tank >= len(RepeaterList):
+	    	logging.info ("tank %d out of range", tank)
+	    	return True
+# skip if this tank was processed within the last second
+	    if currentTime < LastUpdate [tank] + 1:
+	    	return True
+
 	    level = SeeLevelFluidLevelObject.GetValue()
 	    capacity = SeeLevelCapacityObject.GetValue ()
 	    tank2 = SeeLevelTankObject.GetValue()
-
-# if the SeeLevel service is not on the same tank
-# read values again before giving up
-	    if tank != tank2:
-		level = SeeLevelFluidLevelObject.GetValue()
-		capacity = SeeLevelCapacityObject.GetValue ()
-		tank = SeeLevelTankObject.GetValue()
-	    if tank != tank2:
-		return True
 
 	except dbus.DBusException:
 		SeeLevelTankObject = None
 		return True
 
-# fluid type is used to identify the repeater dBus service instance - ignore if out of range
-	if tank < 0 or tank >= len(RepeaterList):
-	    logging.info ("tank %d out of range", tank)
+# if not on the same tank, skip processing
+        if tank != tank2:
 	    return True
 
 # if no repeater service for this tank yet, create it first 
@@ -295,7 +308,7 @@ def CheckSeeLevel():
 
 # forward SeeLevel values to the repeater
 	RepeaterList [tank].UpdateRepeater (level, capacity)
-
+	LastUpdate [tank] = currentTime
 	return True
 
 
