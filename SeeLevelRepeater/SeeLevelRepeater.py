b8a8a0e7f90dab7fc7619b4431c00ad77d98e9b9
kwindrem <58538395+kwindrem@users.noreply.github.com>
2020-04-24 21:07:41 -0700

    changes to make initialization faster
    Code now instantiates all repeater objects, but still waits until messages are received for a specific tank before creating dBus services.

--- SeeLevelRepeater/SeeLevelRepeater.py
+++ SeeLevelRepeater/SeeLevelRepeater.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 # This module creates a dBus tank 'repeater' for handling SeeLevel the NMEA2000 tank sensor system
+# While written for the SeeLevel system, it may work for other CanBus sensors.
 # The Venus NMEA2000 dBus services assume only one tank (aka sensor aka fluid type) per external device
 # so all SeeLevel tanks end up in the same dBus service resulting in each tank overwriting the others
 # To avoid this, individual dBus "repeater' services for each tank are created
@@ -9,18 +10,22 @@
 # This module handles all 6 defined tanks. The SeeLevel N2K sensor system supports 
 # at most 3 tanks (1 = fresh, 2 = gray, 5 = black)
 
-# Modifications to OverviewMobile.qml and TileTank.qml in the GUI are needed to hide the SeeLevel dBus object that rotates between tank data
-# GUI modifications have also been made to alert the operatorto loss of communciaitons with SeeLevel tank
+# Modifications to OverviewMobile.qml in the GUI is needed to hide the SeeLevel dBus object that rotates between tank data
+# Modificaitons to TileTank.qml have also been made to:
+#  alert the operatortor of loss of communciaitons with SeeLevel tank
+#  display custom tank names
+#  remove flashing and add error messages, replacing the tank fullness percentage
+#  squeeze the tile height when necessary to fit more tanks into the available space
 
 # The service daemon insures this repeater module runs at startup and restarts it should it crash 
 # To run this module, a link to the SeeLevelRepeater directory is created in the /service directory
 
 # The Repeater services are created only after data is received from SeeLevel for the related tank
-# to minimize GUI clutter
+# to GUI clutter
 
 # A timeout mechanism control's the repeater's /Connected flag to indicate if the Repeater service is active or not
 # when SeeLevel stops reporting data for a specific tank, it's /Connected flag will eventually be cleared by this timeout mechanism
-# The GUI can then test /Connected to hide stale information from the Tanks column
+# The GUI (TileTank) then tests /Connected to hide stale information from the Tanks column
 
 # SeeLevel sends /FluidType, /Level and /Capacity
 # /FluidType is enumerated consistently with other Victron tanks
@@ -28,17 +33,20 @@
 # /Level is used to report a sensor error, however the NMEA2000 tank driver truncates these values to 0-100%
 # so there is no way to display a sensor error (such as an open in the wiring)
 # SeeLevel reports capacity in liters * 10 and the tank driver converts this to cubic meters used elsewhere in the Venus code
-# /Remaining is calculated locally
+# /Remaining is calculated in the Repeater
 
 # SeeLevel reports information for all tanks about every 3-4 seconds
 # However, it can get into a mode where info for all tanks is sent very quickly, then no activity for ~ 3 seconds
 # Polling for changes can miss data for a specific tank which makes it appear that the tank is not being reported (timeout)
-# For this reason, signal handlers for /FluidType and /Level PropertiesChanged are used to collect tank info
+# For this reason, PropertiesChanged signal handlers for /FluidType and /Level are used to collect tank info
 # But this also is tricky since a signal for a level change may not be issued for every tank.
-# For example, if all tanks are empty, the level signal handler is never called! The same would be true for /FluidLevel if there was only one tank.
-# The signal handlers for /FluidType and /Level store the values received, but processing is deferred for later when the chance is best that they represent
-# a stable type/level pair. Since /FluidType changes with every new message from SeeLevel, that signal is used as the trigger for processing the LAST known values.
-# A background polling task also collects information in the absence of signals but as mentioned above, polling alone can't capture all the data.
+# For example, if all tanks are empty, the level signal handler is never called! 
+# The same would be true for /FluidLevel if there was only one tank.
+# The signal handlers for /FluidType and /Level store the values received,
+# but processing is deferred for later when the chance is best that they represent a stable type/level pair.
+# Since /FluidType changes with every new message from SeeLevel, 
+# that signal is used as the trigger for processing the LAST known /FluidType and /Level values.
+# A background polling task also collects information in the absence of signals.
 # /Capacity changes infrequently, so it is handled in the polling task only (no signal handler).
 # The signal handlers are called from another thread/process so the amount of time spent in these routines is kept to a minimum.
 
@@ -119,12 +127,23 @@ class Repeater:
     global RepeaterTimeout
     global RepeaterTimerPeriod
 
-    RepeaterService = None
+    DbusService = None
+    StartupDelay = True
 
     DbusBus = None
     ServiceName = ""
 
+# local tank values
+# the dBus service is not created until messages are received
+# so the SeeLevel signal handler and background loop set these,
+# triggering service cration in the repeater's background loop
+# repeater background loop then updates the dBus values
+
     Tank = 0
+    Level = 0
+    Capacity = 0
+    UpdateReceived = False
+
     TimeoutCount = 0
 
 
@@ -144,88 +163,55 @@ class Repeater:
 	gobject.timeout_add(RepeaterTimerPeriod, self._update)
 
 
-# check watchdog -- mark service off line if it has timed out
-# RepeaterService may not yet be set up yet if so don't attempt to mark it off line
+# flag value change from external source
 
-    def _update(self):
+    def _handlechangedvalue (self, path, value):
 
-	if self.TimeoutCount > self.RepeaterTimeout:
-# log message only once
-		if self.RepeaterService != None and self.RepeaterService['/Connected'] == 1:
-			self.RepeaterService['/Connected'] = 0
-			logging.warning ("Tank %d is NOT responding", self.Tank)
-	else:
-		self.TimeoutCount += 1
-
-	return True
-
-
-# process value change from external source
-# actual value changes have already occurred 
-# just update /Remaining and restart the watchdog
-
-    def _resetWatchdog (self):
-	if self.RepeaterService['/Connected'] == 0:
-		self.RepeaterService['/Connected'] = 1
-		logging.info ("Tank %d is responding", self.Tank)
-	self.TimeoutCount = 0;
-
-	return
-
-    def _updateRemaining (self):
-	self.RepeaterService['/Remaining'] = self.RepeaterService['/Capacity'] * self.RepeaterService['/Level'] / 100
-	return True
-
-    def _handlechangedvalue(self, path, value):
-
-	self._updateRemaining ()
-	self._resetWatchdog()
+	self.UpdateReceived = True
         return True 
 
 
-# create tank Repeater service if it doesn't yet exist
+# create tank Repeater service
 
-    def _createDbusRepeaterService (self):
-
-	if self.RepeaterService != None:
-		return
+    def _createDbusService (self):
 
 # create a unique service name that puts tanks in the desired order (see note at top of this module)
 	self.ServiceName = RepeaterServiceName % self.Tank
 
 # updated version of VeDbusService (in ext directory) -- see https://github.com/victronenergy/dbus-digitalinputs for new imports
-	self.RepeaterService = VeDbusService (self.ServiceName, bus = self.DbusBus)
+	self.DbusService = VeDbusService (self.ServiceName, bus = self.DbusBus)
 
 # make custom name non-volatile
-        path = '/Settings/Devices/TankRepeater%d' % self.Tank
+        settingsPath = '/Settings/Devices/TankRepeater%d' % self.Tank
 
-        SETTINGS = { 'customname': [path + '/CustomName', '', 0, 0] }
+        SETTINGS = { 'customname': [settingsPath + '/CustomName', '', 0, 0] }
 
         self.settings = SettingsDevice(self.DbusBus, SETTINGS, self.setting_changed)
 
 # Create the objects
 
-	self.RepeaterService.add_path ('/Mgmt/ProcessName', __file__)
-	self.RepeaterService.add_path ('/Mgmt/ProcessVersion', '2.0')
-        self.RepeaterService.add_path ('/Mgmt/Connection', 'dBus')
+	self.DbusService.add_path ('/Mgmt/ProcessName', __file__)
+	self.DbusService.add_path ('/Mgmt/ProcessVersion', '2.0')
+        self.DbusService.add_path ('/Mgmt/Connection', 'dBus')
 
-        self.RepeaterService.add_path ('/DeviceInstance', self.Tank)
-        self.RepeaterService.add_path ('/ProductName', ProductName % self.Tank)
-        self.RepeaterService.add_path ('/ProductId', 0)
-        self.RepeaterService.add_path ('/FirmwareVersion', 0)
-        self.RepeaterService.add_path ('/HardwareVersion', 0)
-        self.RepeaterService.add_path ('/Serial', '')
+        self.DbusService.add_path ('/DeviceInstance', self.Tank)
+        self.DbusService.add_path ('/ProductName', ProductName % self.Tank)
+        self.DbusService.add_path ('/ProductId', 0)
+        self.DbusService.add_path ('/FirmwareVersion', 0)
+        self.DbusService.add_path ('/HardwareVersion', 0)
+        self.DbusService.add_path ('/Serial', '')
 # use numeric values (1/0) not True/False for /Connected to make GUI display correct state
-	self.RepeaterService.add_path ('/Connected', 0)
+	self.DbusService.add_path ('/Connected', 0)
  
-	self.RepeaterService.add_path ('/Level', 0, writeable = True, onchangecallback = self._handlechangedvalue)
-	self.RepeaterService.add_path ('/FluidType', self.Tank, writeable = True, onchangecallback = self._handlechangedvalue)
-	self.RepeaterService.add_path ('/Capacity', 0, writeable = True, onchangecallback = self._handlechangedvalue)
-	self.RepeaterService.add_path ('/Remaining', 0, writeable = True, onchangecallback = self._handlechangedvalue)
+	self.DbusService.add_path ('/Level', 0, writeable = True, onchangecallback = self._handlechangedvalue)
+	self.DbusService.add_path ('/FluidType', self.Tank, writeable = True, onchangecallback = self._handlechangedvalue)
+	self.DbusService.add_path ('/Capacity', 0, writeable = True, onchangecallback = self._handlechangedvalue)
+	self.DbusService.add_path ('/Remaining', 0, writeable = True, onchangecallback = self._handlechangedvalue)
 
-	self.RepeaterService.add_path ('/CustomName', self.get_customname(), writeable = True, onchangecallback = self.customname_changed)
+	self.DbusService.add_path ('/CustomName', self.get_customname(), writeable = True, onchangecallback = self.customname_changed)
 
 	self.TimeoutCount = 0;
+	self.StartupDelay = True
 
 	return
 
@@ -238,7 +224,7 @@ class Repeater:
 
     def setting_changed (self, name, old, new):
         if name == 'customname':
-	    self.RepeaterService['/CustomName'] = new
+	    self.DbusService['/CustomName'] = new
 	return
 
     def customname_changed (self, path, val):
@@ -246,40 +232,72 @@ class Repeater:
         return True
 
 
+# do background processing for this repeater
+# SeeLevel updates Repeater local variables
+# those values are passed to the dBus service as a background operation here
+# the dBus service is created here when the first update for this tank is received
+# the /Connected flag is managed here: True if updates are being received,
+# False if no updates have been received in the timeout period
+
+    def _update(self):
+
+# update has been received - create dBus service if not done previously
+# then update dBus values from local storage
+	if self.UpdateReceived:
+		if self.DbusService == None:
+			self._createDbusService ()
+# do nothing this pass if just created dBus service
+# update flag isn't cleared so the update is processed next pass
+			return True
+
+# update servcie values from local storage
+		self.DbusService['/Level'] = self.Level
+		self.DbusService['/Capacity'] = self.Capacity
+		self.DbusService['/Remaining'] = self.Capacity * self.Level / 100
+		self.UpdateReceived = False
+		self.TimeoutCount = 0
+
+# skip timeout processing if dBus service does not exist
+	if self.DbusService == None:
+		return True
+
+# update connected flag
+	if self.TimeoutCount == 0:
+		if self.DbusService['/Connected'] == 0:
+			self.DbusService['/Connected'] = 1
+			logging.info ("Tank %d is responding", self.Tank)
+
+	if self.TimeoutCount > self.RepeaterTimeout:
+		if self.DbusService['/Connected'] == 1:
+			self.DbusService['/Connected'] = 0
+			logging.warning ("Tank %d is NOT responding", self.Tank)
+	else:
+		self.TimeoutCount += 1
+
+	return True
+
 
 # methods called from the SeeLevel processing to update repeater values
-# the first call to any of these methods creates the service for this Repeater instance
-# preventing unnecessary dBus services from hanging around
 
     def UpdateRepeater (self, level, capacity):
 
-	self._createDbusRepeaterService ()
-
-	self.RepeaterService['/Level'] = level
-	self.RepeaterService['/Capacity'] = capacity
-
-	self._updateRemaining ()
-	self._resetWatchdog()
+	self.Level = level
+	self.Capacity = capacity
+	self.UpdateReceived = True
 	return True
  
     def UpdateLevel (self, level):
 
-	self._createDbusRepeaterService ()
-
 	if level != -99:
-		self.RepeaterService['/Level'] = level
-		self._updateRemaining ()
-		self._resetWatchdog()
+		self.Level = level
+		self.UpdateReceived = True
 	return True
 
     def UpdateCapacity (self, capacity):
 
-	self._createDbusRepeaterService ()
-
 	if capacity != -99:
-		self.RepeaterService['/Capacity'] = capacity
-		self._updateRemaining ()
-		self._resetWatchdog()
+		self.Capacity = capacity
+		self.UpdateReceived = True
 	return True
  
 
@@ -303,8 +321,10 @@ class Repeater:
 #
 # persistent storage for SeeLevel data are created
 # so objects don't have to be fetched each time this process runs
-# GetValue() calls using these pointers sometimes fail for some reason and cause dBus exceptions
-# the exception is trapped and the pointers are reinitialized
+# GetValue() calls using these pointers fails if the SeeLevel messages aren't being received or processed
+# The CanBus processing is tied to the GUI process so if the GUI process dies, SeeLevel messages will not be received.
+# When the SeeLevel dBus service is again present, the connection to it is reset and tank info forwarding continues
+# During this time, a no response error will be displayed.
 
 SeeLevelTankObject = None
 SeeLevelFluidLevelObject = None
@@ -312,7 +332,6 @@ SeeLevelCapacityObject = None
 SeeLevelUniqueName = ""
 LastTank = -99
 LastLevel = -99
-TankNeedsRepeater = -99
 InitNeeded = True
 StuckLevelCount = 0
 
@@ -324,10 +343,10 @@ TheBus = None
 
 RepeaterList =  [None,  None, None, None, None, None ]
 
+
 # check to see if SeeLevel dBus object exists
 # innitialize object pointers if so
 # invalidate object pointers if not
-
 
 def CheckSeeLevel():
 
@@ -343,9 +362,13 @@ def CheckSeeLevel():
 	global LastLevel
 	global TheBus
 	global SeeLevelUniqueName
-	global TankNeedsRepeater
 	global InitNeeded
 	global StuckLevelCount
+
+	tank = -99
+	tank2 = -999
+	level = -99
+	capacity = -99
 
 	try:
 # innitialize object pointers if not done already
@@ -367,40 +390,30 @@ def CheckSeeLevel():
 		capacity = SeeLevelCapacityObject.GetValue ()
 		tank2 = SeeLevelTankObject.GetValue()
 
-# range check tank before using it as an array index
-		if tank >= 0 and tank < len(RepeaterList):
-
-# if no repeater service for this tank yet, create it
-			if RepeaterList [tank] == None:
-				RepeaterList [tank] = Repeater (tank)
-
-# update the repeater's level and capacity values from the poll
-			if tank == tank2:
-				RepeaterList [tank].UpdateRepeater (level, capacity)
-
-# if level change signals are not being received but tank number signals ARE being received
-# the level of all tanks is probalby the same, so jam a polled value into LastLevel
-# wait 10 passes before jaming value to give signals a chance to be received
-				if (LastLevel == -99 and LastTank != -99):
-					StuckLevelCount += 1					
-					if StuckLevelCount >= 10:
-						LastLevel = level
-						logging.info ("No /Level signals have been received - setting LastLevel from polled data") 
-				else:
-					StuckLevelCount = 0
-
 	except dbus.DBusException:
 		if InitNeeded == False:
-			logging.warning ("dBus exception after SeeLevel had previously been resonding") 
+			logging.warning ("No response from SeeLevel after it had been resonding") 
 			LastTank = -99
 			StuckLevelCount = 0
 			InitNeeded = True
 
-# Create a repeater service for a tank received via a signal
-	if (TankNeedsRepeater != -99):
-		if RepeaterList [TankNeedsRepeater] == None:
-			RepeaterList [TankNeedsRepeater] = Repeater (TankNeedsRepeater)
-		TankNeeedsRepeater = -99
+
+# update the repeater's level and capacity values from the poll
+# range check tank before using it as an array index
+	if tank >= 0 and tank < len(RepeaterList) and tank == tank2:
+		RepeaterList [tank].UpdateRepeater (level, capacity)
+
+# if level change signals are not being received but tank number signals ARE being received
+# the level of all tanks is probalby the same, so jam a polled value into LastLevel
+# wait 10 passes before jaming value to give signals a chance to be received
+	if (LastLevel == -99 and LastTank != -99 and level != -99):
+		StuckLevelCount += 1					
+		if StuckLevelCount >= 10:
+			LastLevel = level
+			logging.info ("No /Level signals have been received - setting LastLevel from polled data") 
+	else:
+		StuckLevelCount = 0
+
 	return True
 
 
@@ -411,27 +424,22 @@ def FluidTypeHandler (changes, sender):
 	global SeeLevelUniqueName
 	global LastTank
 	global LastLevel
-	global TankNeedsRepeater
 
 # ignore signal if it's not from the SeeLevel service
 	if sender != SeeLevelUniqueName:
 		return
 
-# Update the repeater based on last tank and level before saving the new tank value
+	tank = int (changes.get ("Value"))
+
+# Update the repeater based on PREVIOUS tank and level before saving the new tank value
 # (level values may not change between tanks)
 # range check tank and level before processing
 # wait until CheckSeeLevel has actually created the reperter service
-	if LastTank >= 0 and LastTank < len(RepeaterList):
-		if RepeaterList [LastTank] != None:
-			RepeaterList [LastTank].UpdateLevel (LastLevel)
-		else:
-# ask CheckSeeLevel to crate a repeater for this tank
-# this is not done in-line to save time in the handler
-			if TankNeedsRepeater == -99:
-				TankNeedsRepeater = LastTank
+	if LastTank >= 0 and LastTank < len(RepeaterList) and LastLevel != -99 and RepeaterList [LastTank] != None:
+		RepeaterList [LastTank].UpdateLevel (LastLevel)
 
 # save new fluid type for processing on next call to this handler
-	LastTank = int (changes.get ("Value"))
+	LastTank = tank
 
 	return
 
@@ -465,6 +473,12 @@ def main():
 	DBusGMainLoop(set_as_default=True)
 
         logging.info (">>>>>>>>>>>>>>>> SeeLevel Repeater Starting <<<<<<<<<<<<<<<<")
+
+# create repeaters for all tanks
+# dBus services are NOT created at this time to save GUI clutter
+	for tank in range (len(RepeaterList)):
+		RepeaterList [tank] = Repeater (tank)
+
 
 # install a signal handler for /FluidType and /Level
 	TheBus = dbus.SystemBus()
